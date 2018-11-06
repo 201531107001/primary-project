6b63c8c46f54c9df5828f377097a76888e5713b7
guoqingming <guoqingming@shunisoft.com>
2018-11-06 10:36:24 +0800

    log4j-demo

---
+++ log4j-demo/src/main/java/com/javalog/Test.java
@@ -0,0 +1,18 @@
+package com.javalog;
+
+import java.util.logging.*;
+public class Test {
+	public static void main(String[] args){
+		Logger log = Logger.getLogger(Test.class.toString());
+		// all→finest→finer→fine→config→info→warning→server→off  
+        // 级别依次升高，后面的日志级别会屏蔽之前的级别  
+        log.setLevel(Level.INFO);  
+        log.finest("finest");  
+        log.finer("finer");  
+        log.fine("fine");  
+        log.config("config");  
+        log.info("info");  
+        log.warning("warning");  
+        log.severe("server");  
+	}
+}
---
+++ log4j-demo/src/main/java/com/log4j/Test.java
@@ -0,0 +1,21 @@
+package com.log4j;
+
+
+import org.apache.log4j.Logger; 
+public class Test {
+	
+	public static void main(String[] args){
+		Logger logger = Logger.getLogger(Test.class);  
+        
+        //使用默认的配置信息，不需要写log4j.properties  
+        //BasicConfigurator.configure();  
+        //设置日志输出级别为info，这将覆盖配置文件中设置的级别  
+        //logger.setLevel(Level.DEBUG);  
+        //下面的消息将被输出  
+        logger.info("this is an "+5+"info");  
+        logger.warn("this is a warn");  
+        logger.error("this is an error");  
+        logger.fatal("this is a fatal");
+        logger.debug("this is a debug");
+	}
+}
